124f62619c28f803dcc80ac481aa24513783460f
Cole Rayo <cole.rayo@gmail.com>
2021-03-27 21:35:10 -0700

    output full html files

--- build.py
+++ build.py
@@ -8,3 +8,6 @@ indexFull = top + index + bottom
 testimonialsFull = top + testimonials + bottom
 contactFull = top + contact + bottom
 
+open("docs/indexFull.html", "w+").write(indexFull)
+open("docs/testimonialsFull.html","w+").write(testimonialsFull)
+open("docs/contactFull.html", "w+").write(contactFull)
