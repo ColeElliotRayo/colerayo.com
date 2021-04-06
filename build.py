fb28840b173c9a756d9cb7c7565cc6332623d6e3
Cole Rayo <cole.rayo@gmail.com>
2021-04-05 19:21:19 -0700

    hw3/phase1

--- build.py
+++ build.py
@@ -1,13 +1,14 @@
-top = open("templates/top.html").read()
-index = open("content/index.html").read()
-testimonials = open("content/testimonials.html").read()
-contact = open("content/contact.html").read()
-bottom = open("templates/bottom.html").read()
+def main():
+    top = open("templates/top.html").read()
+    index = open("content/index.html").read()
+    testimonials = open("content/testimonials.html").read()
+    contact = open("content/contact.html").read()
+    bottom = open("templates/bottom.html").read()
+    indexFull = top + index + bottom
+    testimonialsFull = top + testimonials + bottom
+    contactFull = top + contact + bottom
+    open("docs/index.html", "w+").write(indexFull)
+    open("docs/testimonials.html","w+").write(testimonialsFull)
+    open("docs/contact.html", "w+").write(contactFull)
 
-indexFull = top + index + bottom
-testimonialsFull = top + testimonials + bottom
-contactFull = top + contact + bottom
-
-open("docs/index.html", "w+").write(indexFull)
-open("docs/testimonials.html","w+").write(testimonialsFull)
-open("docs/contact.html", "w+").write(contactFull)
+main()
